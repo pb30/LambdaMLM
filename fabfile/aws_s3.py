9e8d9e13d8b4275b7e0684eef226ca9208c21d93
pb30 <pb30@pb30.com>
2017-10-14 13:23:28 -0500

    #31: Fixed check for existing S3 bucket
    Code from https://boto3.readthedocs.io/en/latest/guide/migrations3.html#accessing-a-bucket

--- fabfile/aws_s3.py
+++ fabfile/aws_s3.py
@@ -17,10 +17,22 @@ s3 = boto3.resource('s3')
 def create_s3_bucket_if_needed(config=None):
     if not config:
         config = check_config()
+        
+        
+    bucket = s3.Bucket(config.s3_bucket)
+    exists = True
     try:
-        bucket = s3.Bucket(config.s3_bucket)
+        s3.meta.client.head_bucket(Bucket=config.s3_bucket)
+    except ClientError as e:
+        # If a client error is thrown, then check that it was a 404 error.
+        # If it was a 404 error, then the bucket does not exist.
+        error_code = int(e.response['Error']['Code'])
+        if error_code == 404:
+            exists = False
+    
+    if exists:
         puts('S3 bucket already exists (created {}).'.format(bucket.creation_date.isoformat()))
-    except ClientError:
+    else:
         puts('Creating S3 bucket...')
         bucket = s3.create_bucket(
                 Bucket=config.s3_bucket,
@@ -28,4 +40,5 @@ def create_s3_bucket_if_needed(config=None):
                     LocationConstraint=config.lambda_region,
                     ),
                 )
+        
     return bucket
